5beaef81d8228253a3ed07530124b53879e2a871
igorkll <frerther@gmail.com>
2026-05-14 20:26:17 +0300

    Update install_weston_configuration.sh

--- install_weston_configuration.sh
+++ install_weston_configuration.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mkdir -p /etc/xdg/weston
+
 cp weston_configuration/weston.ini /etc/xdg/weston/weston.ini
 cp weston_configuration/weston_shell.lua /weston_shell.lua
+
+chmod 755 /etc/xdg/weston/weston.ini
+chmod 755 /weston_shell.lua
