78310f7a330b1e13ed3de07e59d945e216fc3bb3
igorkll <frerther@gmail.com>
2026-05-14 14:19:31 +0300

    ffff

--- install_weston_configuration.sh
+++ install_weston_configuration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p ~/.config/weston
-cp -r weston_configuration/* ~/.config/weston/.
+cp weston_configuration/weston.ini /etc/xdg/weston/weston.ini
+cp weston_configuration/weston_shell.lua /weston_shell.lua
